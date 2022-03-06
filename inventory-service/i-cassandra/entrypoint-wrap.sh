413ac174952e49c0dd3f7774169bc723568494e3
krystyna-garwol <krystyna.garwol@gmail.com>
2022-03-06 15:51:09 +0000

    created schema.cql and updated entrypoint

--- inventory-service/i-cassandra/entrypoint-wrap.sh
+++ inventory-service/i-cassandra/entrypoint-wrap.sh
@@ -1,23 +1,13 @@
 #!/bin/bash
 
-CASSANDRA_KEYSPACE="inventory"
-TABLE="stock_item"
-CQL=`cat createTables.cql`
+wait_for_cassandra() {
+  echo "Waiting for Cassandra"
+  sleep 2
+}
 
-if [[ ! -z "$CASSANDRA_KEYSPACE" && $1 = 'cassandra' ]];
-then
-  CQL="
-  CREATE KEYSPACE $CASSANDRA_KEYSPACE WITH REPLICATION = {'class': 'SimpleStrategy', 'replication_factor': 1};
-  USE $CASSANDRA_KEYSPACE;
-  CREATE TABLE $TABLE(name text, animal text, inStock int, price double, type text, PRIMARY KEY (name, animal));
-  INSERT INTO inventory.stock_item (name, animal, inStock, price, type) VALUES('Purina', 'cat', 50, 3.50, 'cat');
-  "
-
-  until echo $CQL | cqlsh;
-  do
-    echo "Waiting for Cassandra"
-    sleep 2
-  done &
-fi
+until echo "$(< schema.cql)" | cqlsh;
+do
+  wait_for_cassandra
+done &
 
 exec "$@"
