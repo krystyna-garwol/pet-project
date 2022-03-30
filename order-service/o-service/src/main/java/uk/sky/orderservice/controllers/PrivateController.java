e7578dcdc25c8c4de8b3d6af531f16308567e5c6
krystyna-garwol <krystyna.garwol@gmail.com>
2022-03-30 15:09:04 +0100

    os private status controller + unit test

---
+++ order-service/o-service/src/main/java/uk/sky/orderservice/controllers/PrivateController.java
@@ -0,0 +1,16 @@
+package uk.sky.orderservice.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/private")
+public class PrivateController {
+
+    @GetMapping
+    public ResponseEntity<String> getStatus() {
+        return ResponseEntity.ok("ok");
+    }
+}
